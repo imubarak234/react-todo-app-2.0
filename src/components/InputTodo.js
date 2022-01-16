85457202abddc0da74944ba80159e9307f694a45
Mubarak <imubarak2424@gmail.com>
2022-01-16 20:52:23 +0100

    done with forms

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable no-console */
+/* eslint-disable no-alert */
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
@@ -22,10 +23,14 @@ class InputTodo extends Component {
     const hi = this.state;
     const there = this.props;
     e.preventDefault();
-    there.addTodoProps(hi.title);
-    this.setState({
-      title: '',
-    });
+    if (hi.title.trim()) {
+      there.addTodoProps(hi.title);
+      this.setState({
+        title: '',
+      });
+    } else {
+      alert('Please write item');
+    }
   }
 
   render() {
