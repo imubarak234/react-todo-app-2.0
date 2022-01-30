07cba2d6cae5982ecf264303b70593ea6b3228aa
Mubarak <imubarak2424@gmail.com>
2022-01-30 17:31:27 +0100

    styling phase

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,26 +1,38 @@
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable no-console */
+/* eslint-disable react/destructuring-assignment */
 import PropTypes from 'prop-types';
 import React from 'react';
+import styles from './TodoItems.module.css';
 // import PropTypes from 'prop-types';
 
 class TodoItems extends React.Component {
   render() {
     const next = this.props;
+    const { completed, id, title } = this.props.todo;
+    const completedStyle = {
+      fontStyle: 'italic',
+      color: '#595959',
+      opacity: 0.4,
+      textDecoration: 'line-through',
+    };
     return (
-      <li>
+      <li className={styles.item}>
         <input
           type="checkbox"
-          checked={next.todo.completed}
-          onChange={() => next.handleChangesProps(next.todo.id)}
+          checked={completed}
+          className={styles.checkbox}
+          onChange={() => next.handleChangesProps(id)}
         />
-        {next.todo.title}
         <button
           type="button"
-          onClick={() => next.deleteing(next.todo.id)}
+          onClick={() => next.deleteing(id)}
         >
           Delete
         </button>
+        <span style={completed ? completedStyle : null}>
+          {title}
+        </span>
       </li>
     );
   }
